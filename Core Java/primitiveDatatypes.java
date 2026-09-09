20b374642bebe15e08a15192a5d3ffc5b2584e0e
Nkj-718 <jhanitin16@gmail.com>
2026-09-09 23:53:48 +0530

    Learnt about Primitive Datatypes in Java

---
+++ Core Java/primitiveDatatypes.java
@@ -0,0 +1,21 @@
+class primitiveDatatypes{
+    public static void main(String args[]){
+        byte age=20;
+        short id=512;
+        int salary=85000;
+        long contact=9211420420l;
+        double percent=73.8;
+        char highSchoolGrade='C';
+        boolean pass=true;
+        String name="John Doe";
+        System.out.println("Details:-");
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: ₹" + salary);
+        System.out.println("Contact Number: " + contact);
+        System.out.println("Percentage: " + percent);
+        System.out.println("High School Grade: " + highSchoolGrade);
+        System.out.println("Passed the exams?: " + pass);
+    }
+}
