ce6c39df6afc62cc9f14326148b73f10d4928ff8
Nkj-718 <jhanitin16@gmail.com>
2026-09-10 23:34:54 +0530

    Learnt about type conversion and explicit Typecasting in Java

---
+++ Core Java/typeCastingAndConversion.java
@@ -0,0 +1,12 @@
+class typeCastingAndConversion {
+    public static void main(String[] args){
+        int a=278;
+        System.out.println(a);
+        byte b=(byte)a; //typecasting from byte to int.
+        System.out.println(b);
+        char c=(char)a; //typecasting from int to char.
+        String d="Hello";
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
