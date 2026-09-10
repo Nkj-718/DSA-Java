80c7f300e7aa7c7061e12ecf3a69db2f61034b39
Nkj-718 <jhanitin16@gmail.com>
2026-09-10 23:33:28 +0530

    Learnt about literals, binary, and hexadecimal values/representations in Java.

---
+++ Core Java/literals.java
@@ -0,0 +1,10 @@
+class literals {
+    public static void main(String[] args){
+        int a=520; //The value of a variable is Literal.
+        int bin=0b1011011; //Binary Value
+        int hex=0x842E; //Hexadecimal Value
+        System.out.println(a);
+        System.out.println(bin);
+        System.out.println(hex);
+    }    
+}
