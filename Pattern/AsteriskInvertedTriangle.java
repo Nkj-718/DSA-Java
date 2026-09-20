c1160e4e3857a1a967391757179693d1db3280f7
Nkj-718 <jhanitin16@gmail.com>
2026-09-21 01:09:34 +0530

    Inverted Triangle pattern using Asterisk.

---
+++ Pattern/AsteriskInvertedTriangle.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class Pattern{
+    private int size;
+
+    public Pattern(int size){
+        this.size=size;
+    }
+
+    public void printPattern(){
+            for(int i=size-1; i>=0; i--){
+                for(int lSpace=1; lSpace<=(size-i-1); lSpace++){
+                    System.out.print(" ");
+                }
+                for(int stars=(2*i+1); stars>0; stars--){
+                    System.out.print("*");
+                }
+                for(int rSpace=1; rSpace<=(size-i-1); rSpace++){
+                    System.out.print(" ");
+                }
+                System.out.println();
+            }
+    }
+
+}
+
+public class AsteriskInvertedTriangle {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int size=sc.nextInt();
+        Pattern p=new Pattern(size);
+        p.printPattern();
+
+        sc.close();
+    }
+}
