855ff1dcac77716da0e05553f7a23623d75103ae
Nkj-718 <jhanitin16@gmail.com>
2026-09-22 23:11:30 +0530

    Practiced nested loops with numeric right triangle pattern

--- Pattern/NumericRightTriangle.java
+++ Pattern/NumericRightTriangle.java
@@ -8,7 +8,7 @@ class Pattern{
     }
 
     public void printPattern(){
-        for (int i = 1; i <= size; i++){
+        for (int i=1; i<=size; i++){
             for(int j=1; j<=i; j++){
                 System.out.print(j);
             }
