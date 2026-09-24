a62a63b7cd09a1a318bc21e348f47701d4ab9abe
Nkj-718 <jhanitin16@gmail.com>
2026-09-24 23:40:05 +0530

    Hollow Diamond Pattern using spaces and asterisk.

---
+++ Pattern/HollowDiamond.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+class Pattern{
+    private int size;
+
+    public Pattern(int size) {
+        this.size=size;
+        printPattern();
+    }
+
+    public void printPattern() {
+        for (int row=size; row>0; row--) {
+            for (int col=0; col<row; col++) {
+                System.out.print("*");
+            }
+            for (int col=0; col<(2*(size-row)); col++) {
+                System.out.print(" ");
+            }
+            for (int col=row; col>0; col--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int row=0; row<size; row++) {
+            for (int col=0; col<=row; col++) {
+                System.out.print("*");
+            }
+            for (int col=0; col<(2*((size-row)-1)); col++) {
+                System.out.print(" ");
+            }
+            for (int col=0; col<=row; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+public class HollowDiamond{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Rows in one half of the pattern: ");
+        int size=sc.nextInt();
+        Pattern p=new Pattern(size);
+
+        sc.close();
+    }
+}
